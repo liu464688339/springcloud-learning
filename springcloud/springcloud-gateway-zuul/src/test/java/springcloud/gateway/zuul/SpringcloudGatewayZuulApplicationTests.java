4b86c9fbcfee6c92e1346809930a8e89765293db
liu464688339 <464688339@qq.com>
2021-10-09 16:44:51 +0800

    lh
    添加zuul框架代码

---
+++ springcloud/springcloud-gateway-zuul/src/test/java/springcloud/gateway/zuul/SpringcloudGatewayZuulApplicationTests.java
@@ -0,0 +1,16 @@
+package springcloud.gateway.zuul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringcloudGatewayZuulApplication.class)
+class SpringcloudGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
